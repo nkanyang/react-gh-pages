71aa97aa1b4394eee0b6020559f230dc21cbbc5a
jessiehan.au@gmail.com <jessiehan.au@gmail.com>
2020-05-08 18:53:11 +1000

    Updates

---
+++ precache-manifest.d694b65fbcc72cbd9ece4b148335dd30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18987bf3d868702d0c1f016568b569f8",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "2685786633543a166780",
+    "url": "/react-gh-pages/static/css/main.a9fbafbb.chunk.css"
+  },
+  {
+    "revision": "a7d27ee1473b16890883",
+    "url": "/react-gh-pages/static/js/2.4fa61c1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.4fa61c1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2685786633543a166780",
+    "url": "/react-gh-pages/static/js/main.bbea8f0d.chunk.js"
+  },
+  {
+    "revision": "644ac30e43bdb345f267",
+    "url": "/react-gh-pages/static/js/runtime-main.a45d820e.js"
+  }
+]);
